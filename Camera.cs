cb32cf953fc6f77aebe51169a24b53dab53b9c85
sebhotline <45083745+sebhotline@users.noreply.github.com>
2018-11-15 22:44:18 -0500

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,17 @@
+﻿//To make this script work you must assign to to your main camera then select the object you want it to follow
+
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        //By defualt camera will be right in the players location
+        //Use offset to decided how far the camera will be when its following the player
+        transform.position = player.position + offset;
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    // Use this for initialization
+    void Start()
+    {
+        //Setting the movement speed
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move on the x axis                                                     0 is the y axis          Move on the y axis
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, jumpHeight, GetComponent<Rigidbody>().velocity.z);
+        }
+    }
+}
